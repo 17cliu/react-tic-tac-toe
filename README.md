2d4bf9d73accdfc1a2604810e8c6cb178fbb2f3c
Caroline Liu <17cliu@users.noreply.github.com>
2018-07-16 12:11:56 -0400

    use loops to generate board squares, instead of hardcoding them

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 
 - [x] Display the location for each move in the move history list.
 - [ ] Bold the currently selected item in the move list.
-- [ ] Rewrite Board to use two loops to make the squares instead of hardcoding them.
+- [x] Rewrite Board to use two loops to make the squares instead of hardcoding them.
 - [ ] Add a toggle button that lets you sort the moves in either ascending or descending order.
 - [ ] When someone wins, highlight the three squares that caused the win.
 - [x] When no one wins, display a message about the result being a draw.
--- src/index.js
+++ src/index.js
@@ -21,23 +21,14 @@ class Board extends React.Component {
   }
 
   render() {
+    // For each row, for each column, output a square.
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {[...Array(3)].map((v, j) =>
+          <div className="board-row">
+            {[...Array(3)].map((v, i) => this.renderSquare(i + (j * 3)))}
+          </div>
+        )}
       </div>
     );
   }
